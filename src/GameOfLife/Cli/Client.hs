e2a07cb292defd79c6c47dca96a8e045df7f0dd0
Ulisses Bini de Paiva Alves <ulissesbpalves@gmail.com>
2017-03-08 23:24:17 +0000

    update CLI to use terminal's height and width

--- src/GameOfLife/Cli/Client.hs
+++ src/GameOfLife/Cli/Client.hs
@@ -6,6 +6,7 @@ import Control.Concurrent
 import Control.Monad
 import System.Environment
 import qualified System.Console.ANSI as Console
+import qualified System.Console.Terminal.Size as TS
 import qualified GameOfLife.Cli.Drawing as Drawing
 import qualified GameOfLife.Core.Game as Game
 import qualified GameOfLife.Core.Patterns as Patterns
@@ -13,12 +14,14 @@ import qualified GameOfLife.Core.Patterns as Patterns
 main :: IO ()
 main = do
     args <- getArgs
-    refresh . Game.next . Patterns.get . head $ args
+    termSize <- TS.size
+    let Just TS.Window {TS.height=h, TS.width=w} = termSize in begin h w . head $ args
   where
-    refresh previousGame = do
+    begin height width pattern = render height width . Patterns.get $ pattern
+    render height width previousGame = do
         Console.clearScreen
-        Drawing.draw (40,40) game
+        Drawing.draw (height-2,width-1) game
         threadDelay 500000
-        refresh game
+        render height width game
       where
         game = Game.next previousGame
--- src/GameOfLife/Core/Patterns.hs
+++ src/GameOfLife/Core/Patterns.hs
@@ -9,10 +9,14 @@ import GameOfLife.Core.Game (Cell)
 blinker :: [Cell]
 blinker = [(1,0), (1,1), (1,2)]
 
+glider :: [Cell]
+glider = [(0,1),(1,2),(2,0),(2,1),(2,2)]
+
 toad :: [Cell]
 toad = [(3,0), (1,0), (0,1), (3,1), (0,2), (3,2), (1,3)]
 
 get :: String -> [Cell]
 get name = case name of
     "blinker" -> blinker
+    "glider"  -> glider
     "toad"    -> toad
